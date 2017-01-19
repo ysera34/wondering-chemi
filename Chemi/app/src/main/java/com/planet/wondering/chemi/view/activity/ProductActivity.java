da3995b8b2b32adb6c3a139cad2a77c5aa6abdb8
ysera34 <ycyoon34@gmail.com>
2017-01-19 13:09:04 +0900

    add bottom navigation in product activity layout

--- Chemi/app/src/main/java/com/planet/wondering/chemi/view/activity/ProductActivity.java
+++ Chemi/app/src/main/java/com/planet/wondering/chemi/view/activity/ProductActivity.java
@@ -3,20 +3,31 @@ package com.planet.wondering.chemi.view.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.planet.wondering.chemi.R;
+import com.planet.wondering.chemi.common.AppBaseActivity;
 import com.planet.wondering.chemi.model.Product;
 import com.planet.wondering.chemi.model.storage.ProductStorage;
+import com.planet.wondering.chemi.util.helper.BottomNavigationViewHelper;
 import com.planet.wondering.chemi.view.fragment.ProductFragment;
 
 /**
  * Created by yoon on 2017. 1. 18..
  */
 
-public class ProductActivity extends BottomNavigationActivity {
+public class ProductActivity extends AppBaseActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = SearchActivity.class.getSimpleName();
 
@@ -29,6 +40,9 @@ public class ProductActivity extends BottomNavigationActivity {
     private Product mProduct;
 
     private Toolbar mProductToolbar;
+
+    protected BottomNavigationView mBottomNavigationView;
+    public RelativeLayout mBottomNavigationLayout;
 
     public static Intent newIntent(Context packageContext) {
         Intent intent = new Intent(packageContext, ProductActivity.class);
@@ -64,6 +78,11 @@ public class ProductActivity extends BottomNavigationActivity {
 
         setTitle(mProduct.getName());
         mProductToolbar.setSubtitle(mProduct.getBrand());
+
+        mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
+        BottomNavigationViewHelper.disableShiftMode(mBottomNavigationView);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(this);
+        mBottomNavigationLayout = (RelativeLayout) findViewById(R.id.bottom_navigation_layout);
     }
 
     @Override
@@ -72,7 +91,67 @@ public class ProductActivity extends BottomNavigationActivity {
 //        if (mCategoryId > 0) {
 //            setupBottomNavigation(1);
 //        } else {
-//            setupBottomNavigation(0);
+            setupBottomNavigation(0);
 //        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_product, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_faq:
+                Toast.makeText(getApplicationContext(), "action_faq", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_archive:
+                Toast.makeText(getApplicationContext(), "action_archive", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_share:
+                Toast.makeText(getApplicationContext(), "action_share", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                startActivity(SearchActivity.newIntent(getApplicationContext()));
+                break;
+            case R.id.action_category:
+                startActivity(CategoryActivity.newIntent(getApplicationContext()));
+                break;
+            case R.id.action_content:
+                startActivity(ContentActivity.newIntent(getApplicationContext()));
+                break;
+            case R.id.action_dictionary:
+                startActivity(DictionaryActivity.newIntent(getApplicationContext()));
+                break;
+            case R.id.action_member:
+                startActivity(MemberActivity.newIntent(getApplicationContext()));
+                break;
+        }
+        return true;
+    }
+
+    private void setupBottomNavigation(int menuIndex) {
+        mBottomNavigationView.getMenu().getItem(menuIndex).setChecked(true);
+        mBottomNavigationView.getMenu().getItem(menuIndex).setEnabled(false);
+    }
+
+    private void showBottomNavigationView() {
+        mBottomNavigationLayout.animate().translationY(0)
+                .setInterpolator(new DecelerateInterpolator(2));
+    }
+
+    private void hideBottomNavigationView() {
+        mBottomNavigationLayout.animate().translationY(mBottomNavigationLayout.getHeight())
+                .setInterpolator(new AccelerateInterpolator(2));
+    }
+
 }
