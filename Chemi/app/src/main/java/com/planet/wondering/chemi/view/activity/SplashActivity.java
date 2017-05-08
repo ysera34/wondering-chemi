dd6057238c2054d00e759d726b51076d2812ba9b
ysera34 <ycyoon34@gmail.com>
2017-05-08 19:17:29 +0900

    add splash activity

--- Chemi/app/src/main/java/com/planet/wondering/chemi/view/activity/SplashActivity.java
+++ Chemi/app/src/main/java/com/planet/wondering/chemi/view/activity/SplashActivity.java
@@ -3,8 +3,7 @@ package com.planet.wondering.chemi.view.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.Window;
-import android.view.WindowManager;
+import android.util.Log;
 
 import com.planet.wondering.chemi.R;
 import com.planet.wondering.chemi.common.AppBaseActivity;
@@ -15,25 +14,26 @@ import com.planet.wondering.chemi.common.AppBaseActivity;
 
 public class SplashActivity extends AppBaseActivity {
 
-    private static final int SPLASH_TIME_OUT = 1000;
+    private static final String TAG = SplashActivity.class.getSimpleName();
+
+    private static final int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate Start");
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash);
-
-        Intent intent = new Intent(SplashActivity.this, MemberStartActivity.class);
-        startActivity(intent);
-        finish();
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        setContentView(R.layout.activity_splash);
+//
 //        new Handler().postDelayed(new Runnable() {
 //            @Override
 //            public void run() {
-//                Intent intent = new Intent(SplashActivity.this, MemberStartActivity.class);
-//                startActivity(intent);
-//                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-//                finish();
+//                Log.i(TAG, "onCreate postDelayed");
+                Intent intent = new Intent(SplashActivity.this, MemberStartActivity.class);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                startActivity(intent);
+                finish();
 //            }
 //        }, SPLASH_TIME_OUT);
     }
